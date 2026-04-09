cf17521a02f3d306025e2947bc9d19ba0bac53d7
Speed4963 <kim77074963@gmail.com>
2026-04-09 10:48:32 +0900

    백엔드 지도 연결
    'ㅅ'

--- evcharge/src/main/java/com/simplecoding/evcharge/payment/repository/PaymentRepository.java
+++ evcharge/src/main/java/com/simplecoding/evcharge/payment/repository/PaymentRepository.java
@@ -11,7 +11,7 @@ public interface PaymentRepository extends JpaRepository<Payment, Long> {
 
     // insertTime -> createdAt 으로 필드명이 바뀌었다면 메서드명도 수정!
     // 회원별 결제 내역을 최신순으로 조회
-    List<Payment> findByMemberEmailOrderByCreatedAtDesc(String email);
+    List<Payment> findByEmailOrderByCreatedAtDesc(String email);
 
     // 특정 예약 번호에 해당하는 결제 내역 찾기 (신규 추가된 기능)
     List<Payment> findByReservationId(Long reservationId);
