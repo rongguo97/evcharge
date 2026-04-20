30ce53a050b38f7a07e3159a5ba4efaa1ba93d04
kimminaaaa <tbrkcandy@naver.com>
2026-04-20 12:14:25 +0900

    수정
    파일 옮기기

--- evcharge/src/main/java/com/simplecoding/evcharge/reservation/entity/Reservation.java
+++ evcharge/src/main/java/com/simplecoding/evcharge/reservation/entity/Reservation.java
@@ -13,7 +13,42 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 @Builder
 public class Reservation {
+package com.simplecoding.evcharge.reservation.entity;
 
+
+import com.simplecoding.evcharge.station.entity.Station;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+    @Entity
+    @Table(name = "TB_RESERVATION")
+    @Getter
+    @Setter
+    @NoArgsConstructor // ✅ JPA 필수
+    @AllArgsConstructor
+    @Builder
+    public class Reservation {
+
+        @Id
+        @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "res_seq")
+        @SequenceGenerator(name = "res_seq", sequenceName = "SQ_RESERVATION", allocationSize = 1)
+        private Long reservationId;
+        @ManyToOne(fetch = FetchType.LAZY)
+        @JoinColumn(name = "STATION_ID")
+        private Station station;
+
+        private String email;
+
+        private LocalDateTime startTime;
+        private LocalDateTime endTime;
+
+        @Enumerated(EnumType.STRING)
+        private Status status;
+
+
+    }
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "res_seq")
     @SequenceGenerator(name = "res_seq", sequenceName = "SQ_RESERVATION", allocationSize = 1)
