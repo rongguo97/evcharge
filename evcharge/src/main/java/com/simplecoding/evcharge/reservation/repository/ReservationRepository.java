758f9769fd6f47abf5025f4301398a0ac2b0c656
ilkdy1234567-ui <ilkdy1234567@gmail.com>
2026-04-27 11:55:03 +0900

    예약

--- evcharge/src/main/java/com/simplecoding/evcharge/reservation/repository/ReservationRepository.java
+++ evcharge/src/main/java/com/simplecoding/evcharge/reservation/repository/ReservationRepository.java
@@ -3,9 +3,11 @@ package com.simplecoding.evcharge.reservation.repository;
 import com.simplecoding.evcharge.reservation.dto.ReservationDto;
 import com.simplecoding.evcharge.reservation.entity.Reservation;
 import com.simplecoding.evcharge.reservation.entity.Status;
+import org.mapstruct.Mapper;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
@@ -101,4 +103,6 @@ public interface ReservationRepository extends JpaRepository<Reservation, Long> 
             "AND r.status = 'RESERVED'")
     List<Reservation> findReservedSlotsByDate(@Param("chargerId") Long chargerId,
                                               @Param("rDate") String rDate);
-}
+
+    List<Reservation> findByEmailAndStatusIn(String email, List<String> statuses);
+    }
